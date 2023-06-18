0a484defe9af4b1e34e2debdcf746b7ef3779c3c
Nicameow <egoegolin@gmail.com>
2023-06-19 07:37:35 +0800

    Final

--- js3.js
+++ js3.js
@@ -3,7 +3,7 @@ const fixedSidebar = document.querySelector(".fixed-sidebar");
 const music = document.getElementById("music");
 const musicInfo = document.getElementById("music-info");
 let isPlaying = true;
-music.volume = 0.1;
+music.volume = 0.2;
 
 dropdown.addEventListener("mouseover", function () {
     const dropdownContent = this.querySelector(".dropdown-content");
@@ -50,12 +50,43 @@ function hideMusicInfo() {
 }
 music.addEventListener("play", showMusicInfo);
 const imageUrls = [
-        "https://media.gq.com.tw/photos/643e5cc4df66094db9bfa2b9/16:9/w_2560%2Cc_limit/Fr7DmemagAAv8CE.jpeg",
-        "https://media.gq.com.tw/photos/6444fd49c6e4b308c60f24b5/16:9/w_1280,c_limit/%E3%80%90%E9%96%8B%E7%AE%B1%E3%80%91%E3%80%8A%E6%88%91%E6%8E%A8%E7%9A%84%E5%AD%A9%E5%AD%90%E3%80%8B.jpg",
-        "https://imgs.nmplus.hk/wp-content/uploads/2023/04/3hxw8fe_823875664411b4fa29b6.jpeg"
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114365018947596/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114448976334908/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114540139524236/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114677872066600/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114801771819068/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114855337263137/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114924509741126/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120114986753212459/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115047222497400/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115109663096964/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115190881591426/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115277829517373/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115348344156170/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115400865222696/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115458843091104/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115528296570880/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115606277075035/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115715412852859/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115777731838012/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115840302456944/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115891284213800/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115926088568913/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120115999493070918/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116062722199682/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116132075012216/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116176257814659/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116225788354631/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116306289627167/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116367157383208/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116441463652392/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116489102565577/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116609265176676/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116673580630086/image.png",
+        "https://cdn.discordapp.com/attachments/847318233522634772/1120116749946331136/image.png",
     ];
     const aiImage = document.getElementById("aiImage");
-    const interval = 5000;
+    const interval = 1000;
     let currentIndex = 0;
 
     setInterval(() => {
